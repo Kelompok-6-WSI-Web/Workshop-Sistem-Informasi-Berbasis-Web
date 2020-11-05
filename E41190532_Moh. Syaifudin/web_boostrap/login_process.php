a006b14548c12b9fc5fe08ebe7018a1fab9e5e96
syaiifudin <msyaifudin640@gmail.com>
2020-11-05 21:57:47 +0700

    upload tugas praktikum blok

---
+++ E41190532_Moh. Syaifudin/web_boostrap/login_process.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+$email_saya = "msyaifudin640@gmail.com";
+$password_saya = 12345678;
+if((strcasecmp($email_saya, $email)==0)&&($password_saya==$password))
+
+{session_start();
+    $_SESSION['email'] = $email;
+    header("location:index.php?pesan=berhasil");}
+else
+{header("location:login.php?pesan=gagal");}
+?>
