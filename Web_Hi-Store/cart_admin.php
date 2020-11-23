c8a45a1a182c5b094ebe44c278cc8be1bff3a3db
MohammadHidayatullah <mohammadhidayatullah85@gmail.com>
2020-11-23 22:18:56 +0700

    edit home dan menambahkan front-end form cart

--- Web_Hi-Store/cart_admin.php
+++ Web_Hi-Store/cart_admin.php
@@ -1,6 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Katalog</title>
+    </head>
+<body>
 
+<!doctype html>
+<html lang="en">
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -8,53 +16,16 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="fontawesome-free/css/all.min.css">
-    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="cart_admin.css">
 
-    <title>Website Hi - Store</title>
+    <title>Hello, world!</title>
   </head>
-  <body id="page-top">
-
+  <body>
   <?php include('navbar.php'); ?>
-
-    <div class="jumbotron">
-        <div class="container">
-            <h1 class="display-4">SELAMAT DATANG <br> <span class="font-weight-bold"> HIDROPONIK STORE</span> </h1>
-            <p class="lead">fresh from nature</p>
-            <a class="btn btn-primary btn-lg" href="cart_admin.php" role="button">SHOP NOW</a>
-        </div>
-    </div>
-
-    <div class="hiroponik">
-        <div class="container">
-            <h2>Apa itu Hidroponik ? <br> <br></h2>
-            <p>Hidroponik adalah sistem atau teknik budidaya tanaman yang <br>
-            meminimalisir penggunaan tanah. Pada prinsipnya tanaman <br>
-            sebenarnya tidak membutuhkan tanah untuk tumbuh, tanaman <br>
-            hanya membutuhkan mineral dari tanah. 
-            </p>
-        </div>
-    </div>
-
-    <div class="media">
-        <div class="container">
-            <div class="card" style="width: 18rem;">
-            <img src="img/hidro.jpg" class="card-img-top" alt="img/hidro.jpg">
-            </div>
-            <div class="hidro">
-            <h5 class="mt-0">Apa keunggulan metode hidroponik?</h5>
-            Penggunaan lahan lebih efisien. <br>
-            Tanaman berproduksi tanpa menggunakan tanah. <br>
-            Kuantitas dan kualitas panen lebih fresh tanpa pestisida <br>
-            Penggunaan pupuk dan air lebih efisien. <br>
-            Pengendalian hama dan penyakit lebih mudah
-            </div>
-        </div>
-    </div>
-   
+    
     <div class="katalog">
     <div class="container">
-        <h2 class="text-center">Apa saja yang dijual ?</h2>
+        <h2 class="text-left">Sayur</h2>
         <div class="row">
             <div class="col-md-3">
             <div class="card">
@@ -62,6 +33,8 @@
             <div class="card-body">
                 <h5 class="card-title">Sawi Putih</h5>
                 <p class="card-text">Rp. 15.000 / Kg</p>
+                <p class="card-text">Deskripsi Produk</p>
+                <a href="#" class="btn">add to cart</a>
             </div>
             </div>
             </div>
@@ -72,6 +45,8 @@
             <div class="card-body">
                 <h5 class="card-title">Bayam</h5>
                 <p class="card-text">Rp. 10.000 / Kg</p>
+                <p class="card-text">Deskripsi Produk</p>
+                <a href="#" class="btn">add to cart</a>
             </div>
             </div>
             </div>
@@ -82,6 +57,8 @@
             <div class="card-body">
                 <h5 class="card-title">Pakcoy</h5>
                 <p class="card-text">Rp. 15.000 / Kg</p>
+                <p class="card-text">Deskripsi Produk</p>
+                <a href="#" class=" btn">Add to cart</a>
             </div>
             </div>
             </div>
@@ -92,6 +69,8 @@
             <div class="card-body">
                 <h5 class="card-title">Kangkung</h5>
                 <p class="card-text">Rp. 10.000 / Kg</p>
+                <p class="card-text">Deskripsi Produk</p>
+                <a href="#" class="btn">Add to cart</a>
             </div>
             </div>
             </div>
@@ -102,26 +81,94 @@
             <div class="card-body">
                 <h5 class="card-title">Selada</h5>
                 <p class="card-text">Rp. 13.000 / Kg</p>
+                <p class="card-text">Deskripsi Produk</p>
+                <a href="#" class="btn">Add to cart</a>
             </div>
             </div>
             </div>
         </div>
 
 
-    </div>
-</div>
+        <div class="katalog">
+    <div class="container">
+        <h2 class="text-left">Alat</h2>
+        <div class="row">
+            <div class="col-md-3">
+            <div class="card">
+            <img src="img/shop/paralon.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Paralon</h5>
+                <p class="card-text">Rp. 6.000 / m</p>
+                <p class="card-text">Deskripsi Produk</p>
+                <a href="#" class="btn b">Add to cart</a>
+            </div>
+            </div>
+            </div>
 
-    
+            <div class="col-md-3">
+            <div class="card">
+            <img src="img/shop/netpot.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Netpot</h5>
+                <p class="card-text">Rp. 500 / pcs</p>
+                <p class="card-text">Deskripsi Produk</p>
+                <a href="#" class="btn b">Add to cart</a>
+            </div>
+            </div>
+            </div>
 
-    
+            <div class="col-md-3">
+            <div class="card">
+            <img src="img/shop/nutrisi.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Nutrisi</h5>
+                <p class="card-text">Rp. 20.000 / pcs</p>
+                <p class="card-text">Deskripsi Produk</p>
+                <a href="#" class="btn">Add to cart</a>
+            </div>
+            </div>
+            </div>
+
+            <div class="col-md-3">
+            <div class="card">
+            <img src="img/shop/nutrisi2.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Nutrisi</h5>
+                <p class="card-text">Rp. 25.000 / botol</p>
+                <p class="card-text">Deskripsi Produk</p>
+                <a href="#" class="btn">Add to cart</a>
+            </div>
+            </div>
+            </div>
+
+            <div class="col-md-3">
+            <div class="card">
+            <img src="img/shop/starter_kit.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Starter Kit</h5>
+                <p class="card-text">Rp. 60.000 / paket</p>
+                <p class="card-text">Deskripsi Produk</p>
+                <a href="#" class="btn">Add to cart</a>
+            </div>
+            </div>
+            </div>
+        </div>
+
+
+
     <!-- Optional JavaScript; choose one of the two! -->
+
     <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS  -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
-      </body>
+  
+  </body>
 </html>
+    
 </body>
 </html>
