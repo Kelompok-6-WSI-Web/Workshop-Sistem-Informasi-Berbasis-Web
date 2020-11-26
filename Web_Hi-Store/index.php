1a5b3d0d5d96b7cb25663912f1e34eb2f2696692
syaiifudin <msyaifudin640@gmail.com>
2020-11-26 08:05:21 +0700

    upload home

--- Web_Hi-Store/index.php
+++ Web_Hi-Store/index.php
@@ -1,119 +1,119 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="home.css">
+    <script src="https://kit.fontawesome.com/c44947b450.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="fontawesome-free/css/all.min.css">
+    
 
-    <title>Website Hi - Store</title>
+    <title>Hi-Store</title>
   </head>
   <body id="page-top">
-
   <?php include('navbar.php'); ?>
-
-    <div class="jumbotron">
+      <div class="jumbotron">
         <div class="container" >
-            <h1 class="display-4">SELAMAT DATANG <br> <span class="font-weight-bold"> HIDROPONIK STORE</span> </h1>
+            <h1 class="display-4"> <span class="font-weight-bold">SELAMAT DATANG </br> HIDROPONIK STORE</span> </h1>
             <p class="lead">fresh from nature</p>
-            <a class="btn btn-primary btn-lg" href="cart_admin.php" role="button">SHOP NOW</a>
+            <a class="btn btn-primary btn-lg" href="#" role="button">SHOP NOW</a>
         </div>
+      </div>
+      <div class="pengertian">
+        <div class="container">
+          <h2>Apa itu Hidroponik?</h2>
+          <p class="lead">Hidroponik adalah sistem atau teknik budididaya tanaman yang </br>
+            meminimilisir penggunaan tanah. Pada prinsipnya tanaman </br>
+            sebenarnya tidak membutuhkan tanah untuk tumbuh, tanaman </br>
+            hanya membutuhkan mineral dari tanah</p>
+        </div>
+      </div>
+
+    <div class="keunggulan">
+      <div class="container">
+        <img src="img/keunggulan.jpg" alt="" class="gambar"> 
+        Apa Keunggulan Metode Hidroponik?
+        </div>
+      </div>
+    </div>
+    
+    <div class="dijual text-white">
+      <div class="container">
+          <center>
+            <h2>Apa saja yang dijual? </h2>
+          </center>
+      </div>
     </div>
 
-    <div class="hiroponik">
-        <div class="container">
-            <h2>Apa itu Hidroponik ? <br> <br></h2>
-            <p>Hidroponik adalah sistem atau teknik budidaya tanaman yang <br>
-            meminimalisir penggunaan tanah. Pada prinsipnya tanaman <br>
-            sebenarnya tidak membutuhkan tanah untuk tumbuh, tanaman <br>
-            hanya membutuhkan mineral dari tanah. 
-            </p>
-        </div>
-    </div>
 
-    <div class="media">
-        <div class="container">
-            <div class="card" style="width: 18rem;">
-            <img src="img/hidro.jpg" class="card-img-top" alt="img/hidro.jpg">
-            </div>
-            <div class="hidro mx-auto">
-            <h5 class="definisi mx-auto">Apa keunggulan metode hidroponik?</h5>
-            Penggunaan lahan lebih efisien. <br>
-            Tanaman berproduksi tanpa menggunakan tanah. <br>
-            Kuantitas dan kualitas panen lebih fresh tanpa pestisida <br>
-            Penggunaan pupuk dan air lebih efisien. <br>
-            Pengendalian hama dan penyakit lebih mudah
-            </div>
-        </div>
-    </div>
-   
-    <div class="katalog">
     <div class="container">
-        <h2 class="text-center">Apa saja yang dijual ?</h2>
+      <div class="katalog">
         <div class="row">
-            <div class="col-md-3">
-            <div class="card">
-            <img src="img/shop/sawi.jpg" class="card-img-top" alt="...">
+          <div class="col-md-3">
+          <div class="card">
+            <img src="img/sayuran/bayam.png" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">Sawi Putih</h5>
-                <p class="card-text">Rp. 15.000 / Kg</p>
+              <center>
+              <h5 class="card-title">Bayam </h5>
+             </center>
             </div>
-            </div>
-            </div>
-
-            <div class="col-md-3">
+          </div>
+          </div>
+          <div class="col-md-3">
             <div class="card">
-            <img src="img/shop/bayam.png" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Bayam</h5>
-                <p class="card-text">Rp. 10.000 / Kg</p>
-            </div>
-            </div>
-            </div>
-
-            <div class="col-md-3">
-            <div class="card">
-            <img src="img/shop/pakcoy.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Pakcoy</h5>
-                <p class="card-text">Rp. 15.000 / Kg</p>
-            </div>
-            </div>
-            </div>
-
-            <div class="col-md-3">
-            <div class="card">
-            <img src="img/shop/kangkung.png" class="card-img-top" alt="...">
-            <div class="card-body">
+              <img src="img/sayuran/kangkung.png" class="card-img-top" alt="...">
+              <div class="card-body">
+                <center>
                 <h5 class="card-title">Kangkung</h5>
-                <p class="card-text">Rp. 10.000 / Kg</p>
+                </center>
+              </div>
             </div>
             </div>
-            </div>
-
             <div class="col-md-3">
-            <div class="card">
-            <img src="img/shop/selada.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Selada</h5>
-                <p class="card-text">Rp. 13.000 / Kg</p>
-            </div>
-            </div>
+              <div class="card">
+                <img src="img/sayuran/pakcoy.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <center>
+                    <h5 class="card-title">Pakcoy</h5>
+                  </center>
+                </div>
+              </div>
+              </div>
+            <div class="col-md-3">
+             <div class="card">
+              <img src="img/sayuran/sawiputih.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <center>
+                  <h5 class="card-title">Sawi</h5>
+                  </center>
+                </div>
+              </div>
             </div>
         </div>
-
-
+      </div>
     </div>
-</div>
 
-    
+    <div class="pengertian">
+      <div class="container">
+        <h2>Visit Us</h2><br>
+        <h3>
+          <i class="fab fa-instagram ml-5"></i> @histore
+          <i class="fab fa-facebook ml-5"></i>histore
+        </h3>
+      </div>
+    </div>
 
-    
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="js/bootstrap.min.js"></script>
+
     <!-- Optional JavaScript; choose one of the two! -->
     <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
@@ -121,7 +121,5 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
-      </body>
-</html>
-</body>
+  </body>
 </html>
