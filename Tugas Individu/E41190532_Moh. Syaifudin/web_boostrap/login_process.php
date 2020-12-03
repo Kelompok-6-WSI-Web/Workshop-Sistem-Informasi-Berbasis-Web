a8f77ab5d222403e96631e78a10d399f54b0923f
syaiifudin <msyaifudin640@gmail.com>
2020-12-03 13:02:13 +0700

    upload tugas login menggunakan database

--- Tugas Individu/E41190532_Moh. Syaifudin/web_boostrap/login_process.php
+++ Tugas Individu/E41190532_Moh. Syaifudin/web_boostrap/login_process.php
@@ -1,15 +1,21 @@
 <?php
 session_start();
 
-$email = $_POST['email'];
-$password = $_POST['password'];
-$email_saya = "msyaifudin640@gmail.com";
-$password_saya = 12345678;
-if((strcasecmp($email_saya, $email)==0)&&($password_saya==$password))
+include 'koneksi.php';
 
-{session_start();
-    $_SESSION['email'] = $email;
-    header("location:index.php?pesan=berhasil");}
-else
-{header("location:login.php?pesan=gagal");}
+$email=$_POST['email'];
+$password=$_POST['password'];
+$querySql = mysqli_query($koneksi,"SELECT * FROM akun WHERE email='$email'");
+$data = mysqli_fetch_array($querySql);
+
+if ($email==$data['email']){
+    if ($password == $data ['password']){
+        header("location:index.php?pesan=berhasil");
+    } else{
+        header("location:login.php?pesan=passwordsalah");
+    }
+}else{
+    
+    header("location:login.php?pesan=emailsalah");
+}
 ?>
