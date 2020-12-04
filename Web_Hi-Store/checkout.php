d790bdad1987ee5b320eeda301a29bb9144862b5
Sintanwd <sintanur096@gmail.com>
2020-12-04 17:39:51 +0700

    menambahkan di file checkout
    menambahkan di fitur checkout

--- Web_Hi-Store/checkout.php
+++ Web_Hi-Store/checkout.php
@@ -42,6 +42,7 @@ include 'koneksi.php';
             </thead>
             <tbody>
                 <?php $nomor=1; ?>
+                <?php $totalbelanja=0; ?>
                 <?php foreach ($_SESSION['keranjang'] as $id_produk => $jumlah): ?>
         
         <!-- menampilkan produk -->
@@ -49,6 +50,7 @@ include 'koneksi.php';
                 $ambil = $koneksi->query("select * from produk where id_produk = '$id_produk'"); 
                 $pecah = $ambil->fetch_assoc();
                 $subharga = $pecah["harga_produk"]*$jumlah;
+                
 
                 // echo "<pre>";
                 // print_r($pecah);
@@ -63,10 +65,74 @@ include 'koneksi.php';
                     <td> Rp. <?php echo number_format($subharga); ?></td>
                 </tr>
                 <?php $nomor++; ?>
+                <?php $totalbelanja+=$subharga; ?>
                 <?php endforeach ?>
             </tbody>
+            <tfoot>
+                <tr>
+                    <th colspan="4">Total Belanja</th>
+                    <th>Rp. <?php echo number_format($totalbelanja) ?> </th>
+                </tr>
+            </tfoot>
         </table>
-        <a href="checkout.php" class="btn btn-primary">Bayar</a>
+
+<!--form pembelian -->
+
+        <form method="post">
+            <div class="row">
+            <div class="col-md-4">
+                    <select class="form-control" name="id_ongkir">
+                        <option value="">Pilih Ongkos Kirim</option>
+                        <?php
+                        $ambil = $koneksi->query("SELECT * FROM ongkir");
+                        while($perongkir = $ambil->fetch_assoc()){
+                        ?>
+                        <option value="<?php echo $perongkir["id_ongkir"] ?>">
+                            <?php echo $perongkir['nama_kecamatan'] ?> -
+                            <?php echo $perongkir['tarif'] ?> 
+                        </option>
+                        
+                        <?php
+                            }
+                        ?>
+                    </select>
+            </div>
+            </div>
+            
+        </form>
+        <br>
+        <a href="checkout.php" class="btn btn-primary">Checkout</a>
+
+        <?php 
+        if (isset($_POST["checkout"]))
+        {
+            $id_ongkir = $_POST["id_ongkir"];
+            $tanggal_pembelian = date("Y-m-d");
+
+            $ambil = $koneksi->query("SELECT * FROM ongkir WHERE id_ongkir='$id_ongkir'");
+            $arrayongkir = $ambil->fetch_assoc();
+            $tarif = $arrayongkir['tarif'];
+
+            $total_pembelian = $totalbelanja + $tarif;
+
+            //menyimpan data ke tabel pembelian
+            $koneksi->query("INSERT INTO pembelian (
+            id_ongkkir,tanggal_pembelian,total_pembelian)
+            VALUES ('$id_ongkir','$tanggal_pembelian','$total_pembelian')"
+            );
+
+            //mendapatkan id_pembelian barusan terjadi
+            $id_pembelian_barusan = $koneksi->insert_id;
+
+            foreach ($_SESSION["keranjang"] as $id_produk => $jumlah)
+            {
+                $koneksi->query("INSERT INTO pembelian_produk (id_pembelian,id_produk,jumlah)
+                VALUES ('$id_pembelian_barusan','$id_produk','$jumlah') ");
+            }
+
+
+        }
+        ?>
     </div>
 </section>
 
