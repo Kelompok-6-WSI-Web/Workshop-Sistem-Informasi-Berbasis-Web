84a5cfa859c49a286dc0a74093cbdd43e40e3f39
Moh. Syaifudin <msyaifudin640@users.noreply.github.com>
2020-12-15 20:32:47 +0700

    upload index + news

--- Web_Hi-Store/index.php
+++ Web_Hi-Store/index.php
@@ -12,7 +12,8 @@ include 'koneksi.php';
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="home.css">
-    <script src="js/c44947b450.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="news.css">
+    <script src="https://kit.fontawesome.com/c44947b450.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="fontawesome-free/css/all.min.css">
     
 
@@ -23,20 +24,7 @@ include 'koneksi.php';
       <div class="jumbotron">
         <div class="container" >
             <h1 class="display-4"> <span class="font-weight-bold">SELAMAT DATANG </br> HIDROPONIK STORE</span> </h1>
-            <?php
-                if (isset($_GET['pesan'])){
-                            $pesan = $_GET['pesan'];
-                            if ($pesan == "berhasil") {
-                              ?>
-                              <div class="alert alert-success">
-                                <strong>Succes!</strong> Anda berhasil login.
-                              </div>
-                              <?php
-                            }
-                          }
-                ?>
             <p class="lead">fresh from nature</p>
-            
             <a class="btn btn-primary btn-lg" href="cart_user.php" role="button">SHOP NOW</a>
         </div>
       </div>
@@ -49,11 +37,21 @@ include 'koneksi.php';
             hanya membutuhkan mineral dari tanah</p>
         </div>
       </div>
-      
+
     <div class="keunggulan">
       <div class="container">
-        <img src="img/keunggulan.jpg" alt="" class="gambar"> 
-      Keunggulan menanam Hidroponik
+        <div class="row">
+          <div class="col-md-4">
+            <img src="img/keunggulan.jpg" alt="" class="gambar">
+          </div>
+          <div class="col-md-8">
+              <h5 class="card-title">Apa Keunggulan Metode Hidroponik?</h5>
+              <p class="card-text">Penggunaan lahan lebih efisien <br>
+              Tanaman berproduksi tanpa menggunakan tanah <br>
+              Kuantitas dan Kualitas panen lebih segar dan sehat tanpa pestisida <br>
+              Penggunaan pupuk dan air lebih efisien <br>
+              Pengendalian hama dan penyakit lebih mudah </p>
+          </div>
         </div>
       </div>
     </div>
@@ -123,9 +121,6 @@ include 'koneksi.php';
         </h3>
       </div>
     </div>
-    <div class="footer">
-    <p align="center">Copyright @ Hi-Dev</p>
-   </div>
 
 
 
@@ -136,9 +131,10 @@ include 'koneksi.php';
 
     <!-- Optional JavaScript; choose one of the two! -->
     <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-    <script src="js/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-    <script src="js/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
   </body>
 </html>
---
+++ Web_Hi-Store/news.php
@@ -0,0 +1,57 @@
+<?php
+include 'koneksi.php';
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="news.css">
+    <script src="https://kit.fontawesome.com/c44947b450.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="fontawesome-free/css/all.min.css">
+    
+
+    <title>Hi Store</title>
+  </head>
+  <body id="page-top">
+  <?php include('navbar.php'); ?>
+    <div class="container">
+        <div class="berita">
+          <div class="card mb-3">
+              <img class="card-img-top" src="img/news/banner1.jpg" alt="Card image cap">
+              <div class="card-body">
+                  <h5 class="card-title">SALE HARI NATAL </h5>
+                  <p class="card-text">Dalam rangka memperingati Hari Raya Natal dan Akhir Tahun Hi-Store mengadakan sale besar-besaran sampai 50% untuk semua jenis sayuran 
+                  yang ada di halaman shop. Sale dimulai dari tanggal 25 Desember- 02 Januari 2021 yuk buruan diborong jangan sampai kehabisan yahh !</p>
+                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              </div>
+          </div>
+          <div class="card mb-3">
+              <img class="card-img-top" src="img/news/banner2.jpg" alt="Card image cap">
+              <div class="card-body">
+                  <h5 class="card-title">HIDROPONIK 100% ORGANIK </h5>
+                  <p class="card-text">Sayur hidroponik merupakan 
+                  sayuran sehat yang tidak ditanam di tanah. Alih-alih menggunakan tanah, 
+                  air mineral digunakan sebagai media tanam. Makanya, tanaman sayur hidroponik bisa ditanam 
+                  di area yang sempit sekalipun. Sayuran hidroponik juga biasanya lebih aman dari s
+                  erangan hama sehingga bebas pestisida.</p>
+                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+              </div>
+          </div>
+        </div>
+    </div>
+    
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
