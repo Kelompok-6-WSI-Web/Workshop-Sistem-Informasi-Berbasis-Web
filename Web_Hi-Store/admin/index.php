0caf5f1e3521f505657ba554b21efc35e34f5ff5
Moh. Syaifudin <msyaifudin640@users.noreply.github.com>
2020-12-17 16:20:47 +0700

    adminn up

--- Web_Hi-Store/admin/index.php
+++ Web_Hi-Store/admin/index.php
@@ -2,127 +2,133 @@
 include 'koneksi.php';
 ?>
 
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="news.css">
+    <script src="https://kit.fontawesome.com/c44947b450.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="fontawesome-free/css/all.min.css">
+    
 
-    <title>Website Hi - Store</title>
-     <style>
-        #mainnav {
-            background-color: #99B8A4;
-        }
-    </style>
+    <title>Hi-Store</title>
   </head>
   <body id="page-top">
+  <?php include('navbar.php'); ?>
+      <div class="jumbotron">
+        <div class="container" >
+            <h1 class="display-4"> <span class="font-weight-bold">SELAMAT DATANG </br> HIDROPONIK STORE</span> </h1>
+            <p class="lead">fresh from nature</p>
+            <a class="btn btn-primary btn-lg" href="cart_user.php" role="button">SHOP NOW</a>
+        </div>
+      </div>
+      <div class="pengertian">
+        <div class="container">
+          <h2>Apa itu Hidroponik?</h2>
+          <p class="lead">Hidroponik adalah sistem atau teknik budididaya tanaman yang </br>
+            meminimilisir penggunaan tanah. Pada prinsipnya tanaman </br>
+            sebenarnya tidak membutuhkan tanah untuk tumbuh, tanaman </br>
+            hanya membutuhkan mineral dari tanah</p>
+        </div>
+      </div>
 
-<!-- navbar -->
-<nav class="navbar navbar-expand-lg navbar-primary fixed-top " id="mainnav">
+    <div class="keunggulan">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <img src="img/keunggulan.jpg" alt="" class="gambar">
+          </div>
+          <div class="col-md-8">
+              <h5 class="card-title">Apa Keunggulan Metode Hidroponik?</h5>
+              <p class="card-text">Penggunaan lahan lebih efisien <br>
+              Tanaman berproduksi tanpa menggunakan tanah <br>
+              Kuantitas dan Kualitas panen lebih segar dan sehat tanpa pestisida <br>
+              Penggunaan pupuk dan air lebih efisien <br>
+              Pengendalian hama dan penyakit lebih mudah </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="dijual text-white">
+      <div class="container">
+          <center>
+            <h2>Apa saja yang dijual? </h2>
+          </center>
+      </div>
+    </div>
+
+
     <div class="container">
-    <a class="navbar-brand text-white" href="#">Hi - Store</a>
-    <button class="navbar-toggler navbar-togler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-				
-
-             <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active">
-                    <a class="nav-link text-white" href="home.php"> HOME <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link text-white" href="index.php?halaman=produk"> PRODUK </a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link text-white" href="index.php?halaman=about"> ABOUT </a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link text-white" href="index.php?halaman=news"> NEWS </a>
-                    </li>
-                    <li>
-                        <a href="cart_user.php"><i class="fas fa-shopping-cart text-white ml-3 mt-3"></i></a>            
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="fas fa-user text-white ml-3 mt-2"></i>
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    
-                            <a class="dropdown-item" href="login.php?halaman=signin">Sign In</a>
-                            <a class="dropdown-item" href="register.php?halaman=signup">Sign Up</a>
-                            <div class="dropdown-divider"></div>
-                            
-                            <a class="dropdown-item" href="logout.php?halaman=logout">Log Out</a>
-                        </div>
-                        </li>
-                </ul>
-               
+      <div class="katalog">
+        <div class="row">
+          <div class="col-md-3">
+          <div class="card">
+            <img src="img/sayuran/bayam.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <center>
+              <h5 class="card-title">Bayam </h5>
+             </center>
+            </div>
+          </div>
+          </div>
+          <div class="col-md-3">
+            <div class="card">
+              <img src="img/sayuran/kangkung.png" class="card-img-top" alt="...">
+              <div class="card-body">
+                <center>
+                <h5 class="card-title">Kangkung</h5>
+                </center>
+              </div>
+            </div>
+            </div>
+            <div class="col-md-3">
+              <div class="card">
+                <img src="img/sayuran/pakcoy.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <center>
+                    <h5 class="card-title">Pakcoy</h5>
+                  </center>
+                </div>
+              </div>
+              </div>
+            <div class="col-md-3">
+             <div class="card">
+              <img src="img/sayuran/sawiputih.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <center>
+                  <h5 class="card-title">Sawi</h5>
+                  </center>
+                </div>
+              </div>
             </div>
         </div>
-    </nav>
-            
-        </nav>  
-        <!-- /. NAV SIDE  -->
-        <div id="page-wrapper" >
-            <div id="page-inner">
-                <?php
-                if (isset($_GET['halaman']))
-                {
-                    if($_GET['halaman']=="produk")
-                    {
-                        include'produk.php';
-                    }
-                    elseif($_GET['halaman']=="about")
-                    {
-                        include 'about.php';
-                    }
-                    elseif($_GET['halaman']=="news")
-                    {
-                        include 'news.php';
-                    }
-                    elseif($_GET['halaman']=="detail")
-                    {
-                        include 'detail.php';
-                    }
-                    elseif($_GET['halaman']=="tambahproduk")
-                    {
-                        include 'tambahproduk.php';
-                    }
-                    elseif($_GET['halaman']=="hapusproduk")
-                    {
-                        include 'hapusproduk.php';
-                    }
-                    elseif($_GET['halaman']=="ubahproduk")
-                    {
-                        include 'ubahproduk.php';
-                    }elseif($_GET['halaman']=="signin")
-                    {
-                        include 'login.php';
-                    }elseif($_GET['halaman']=="logout")
-                    {
-                        include 'logout.php';
-                    }
-                    else 
-                    {
-                        include 'home.php';
-                    }
-                }
-                ?>
-            </div>     
+      </div>
+    </div>
+
+    <div class="pengertian">
+      <div class="container">
+        <h2>Visit Us</h2><br>
+        <h3>
+          <i class="fab fa-instagram ml-5"></i> @histore
+          <i class="fab fa-facebook ml-5"></i>histore
+        </h3>
+      </div>
+    </div>
 
 
 
-    
+    <!-- Optional JavaScript; choose one of the two! -->
 
-    
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="js/bootstrap.min.js"></script>
+
     <!-- Optional JavaScript; choose one of the two! -->
     <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
@@ -130,7 +136,5 @@ include 'koneksi.php';
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
-      </body>
-</html>
-</body>
+  </body>
 </html>
--- Web_Hi-Store/admin/navbar.php
+++ Web_Hi-Store/admin/navbar.php
@@ -1,120 +1,74 @@
-<?php
-// koneksi
-$koneksi = new mysqli("localhost", "root","", "hi_store");
-?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-    <link rel="stylesheet" href="fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="home.css">
+    <script src="https://kit.fontawesome.com/c44947b450.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="fontawesome-free/css/all.min.css">
 
-    <title>Website Hi - Store</title>
-    <style>
-        #mainnav {
-            background-color: #99B8A4;
-        }
-    </style>
-
+    <title>Nabvbar</title>
   </head>
   <body id="page-top">
-
-<!-- navbar -->
-<nav class="navbar navbar-expand-lg navbar-primary fixed-top " id="mainnav">
-    <div class="container">
-    <a class="navbar-brand text-white" href="#">Hi - Store</a>
-    <button class="navbar-toggler navbar-togler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainnav">
+        <div class="container">
+        <a class="navbar-brand" href="index.php"><img src="img/logo.png" height="30px" width="30px"></a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
-    </button>
-				
-
-             <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active">
-                    <a class="nav-link text-white" href="index.php"> HOME </a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link text-white" href="index.php?halaman=produk"> PRODUK </a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link text-white" href="index.php?halaman=about"> ABOUT </a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link text-white" href="index.php?halaman=news"> NEWS </a>
-                    </li>
-                    <li>
-                        <a href="cart_user.php"><i class="fas fa-shopping-cart text-white ml-3 mt-3"></i></a>            
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="fas fa-user text-white ml-3 mt-2"></i>
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    
-                            <a class="dropdown-item" href="login.php?halaman=signin">Sign In</a>
-                            <a class="dropdown-item" href="register.php?halaman=signup">Sign Up</a>
-                            <div class="dropdown-divider"></div>
-                            
-                            <a class="dropdown-item" href="logout.php?halaman=logout">Log Out</a>
-                        </div>
-                        </li>
-                </ul>
-               
-            </div>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav mx-auto">
+            <li class="nav-item active">
+            <a class="nav-link js-scroll-trigger text-white ml-4" href="index.php">HOME<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link js-scroll-trigger text-white ml-4" href="cart_user.php">SHOP</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link js-scroll-trigger text-white ml-4" href="about.php">ABOUT</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link js-scroll-trigger text-white ml-4" href="news.php">NEWS</a>
+            </li>
+        </ul>
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+            <a href="keranjang.php">
+            <i class="fas fa-cart-plus text-white ml-3 mt-3"></i></a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <i class="fas fa-user text-white ml-3 mt-2"></i>
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+        
+                <a class="dropdown-item" href="login.php">Sign In</a>
+                <a class="dropdown-item" href="register.php">Sign Up</a>
+                <div class="dropdown-divider"></div>
+                  
+                <a class="dropdown-item" href="logout.php">Log Out</a>
+              </div>
+             
+            </li>
+        </ul>
         </div>
+        </div>  
     </nav>
-            
-        </nav>  
-        <!-- /. NAV SIDE  -->
-        <div id="page-wrapper" >
-            <div id="page-inner">
-                <?php
-                if (isset($_GET['halaman']))
-                {
-                    if($_GET['halaman']=="produk")
-                    {
-                        include'produk.php';
-                    }
-                    elseif($_GET['halaman']=="about")
-                    {
-                        include 'about.php';
-                    }
-                    elseif($_GET['halaman']=="news")
-                    {
-                        include 'news.php';
-                    }
-                    elseif($_GET['halaman']=="detail")
-                    {
-                        include 'detail.php';
-                    }
-                    elseif($_GET['halaman']=="tambahproduk")
-                    {
-                        include 'tambahproduk.php';
-                    }
-                    elseif($_GET['halaman']=="hapusproduk")
-                    {
-                        include 'hapusproduk.php';
-                    }
-                    elseif($_GET['halaman']=="ubahproduk")
-                    {
-                        include 'ubahproduk.php';
-                    }elseif($_GET['halaman']=="signin")
-                    {
-                        include 'login.php';
-                    }elseif($_GET['halaman']=="logout")
-                    {
-                        include 'logout.php';
-                    }
-                    else 
-                    {
-                        include 'index.php';
-                    }
-                }
-                ?>
-            </div>     
+    
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    
+  </body>
+</html>
