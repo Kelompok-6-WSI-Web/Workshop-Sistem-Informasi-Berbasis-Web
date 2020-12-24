77de0477c29f764445d1243ff19150f6992060ad
MohammadHidayatullah <mohammadhidayatullah85@gmail.com>
2020-12-24 11:52:10 +0700

    update lagi

--- Web_Hi-Store/login_process.php
+++ Web_Hi-Store/login_process.php
@@ -10,10 +10,25 @@
     if($email == $data['email']){
         if($password == $data['password']){
             if ($data['id_kategori'] == 1) {
-				$_SESSION['user'] = "coba";
+                $_SESSION['user'] = "coba";
+                
+                $_SESSION['id'] = $data['id_user'];
+                $_SESSION['email'] = $email;
+                $_SESSION['nama'] = $data['nama'];
+                $_SESSION['no_ho'] = $data['no_ho'];
+                $_SESSION['alamat'] = $data['alamat'];
+                $_SESSION['nama_kategori'] = "admin";
+                
 				header("location:admin/home.php");
 			}elseif ($data['id_kategori'] == 2) {
-				$_SESSION['user'] = "coba";
+                $_SESSION['user'] = "coba";
+                
+                $_SESSION['id'] = $data['id_user'];
+                $_SESSION['email'] = $email;
+                $_SESSION['nama'] = $data['nama'];
+                $_SESSION['no_ho'] = $data['no_ho'];
+                $_SESSION['alamat'] = $data['alamat'];
+                $_SESSION['nama_kategori'] = "customer";
                 header("location:home_customer.php");
             }
     }else{
