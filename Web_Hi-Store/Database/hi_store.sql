0c96d76049d204193ad19c79351804ed9ed3620e
MohammadHidayatullah <mohammadhidayatullah85@gmail.com>
2020-12-24 14:39:34 +0700

    upload database terbaru

--- Web_Hi-Store/Database/hi_store.sql
+++ Web_Hi-Store/Database/hi_store.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 27, 2020 at 02:12 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.3
+-- Generation Time: Dec 24, 2020 at 07:51 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -44,6 +43,125 @@ INSERT INTO `jenis` (`id_jenis`, `nama_jenis`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(11) NOT NULL,
+  `nama_kategori` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `nama_kategori`) VALUES
+(1, 'admin'),
+(2, 'customer');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ongkir`
+--
+
+CREATE TABLE `ongkir` (
+  `id_ongkir` int(11) NOT NULL,
+  `nama_kecamatan` varchar(100) NOT NULL,
+  `tarif` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `ongkir`
+--
+
+INSERT INTO `ongkir` (`id_ongkir`, `nama_kecamatan`, `tarif`) VALUES
+(1, 'Kalisat', 20000),
+(2, 'Sumbersari', 15000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembelian`
+--
+
+CREATE TABLE `pembelian` (
+  `id_pembelian` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_ongkir` int(11) NOT NULL,
+  `tanggal_pembelian` date NOT NULL,
+  `total_pembelian` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pembelian`
+--
+
+INSERT INTO `pembelian` (`id_pembelian`, `id_user`, `id_ongkir`, `tanggal_pembelian`, `total_pembelian`) VALUES
+(1, 2, 1, '2020-12-04', '180000'),
+(2, 2, 2, '2020-12-16', '200000'),
+(3, 2, 0, '2020-12-24', '35000'),
+(4, 2, 1, '2020-12-24', '40000'),
+(5, 2, 2, '2020-12-24', '80000'),
+(6, 2, 1, '2020-12-24', '100000'),
+(7, 2, 1, '2020-12-24', '50000'),
+(8, 3, 1, '2020-12-24', '50000'),
+(9, 3, 1, '2020-12-24', '35000'),
+(10, 2, 1, '2020-12-24', '35000'),
+(11, 2, 1, '2020-12-24', '65000'),
+(12, 2, 1, '2020-12-24', '55000'),
+(13, 2, 1, '2020-12-24', '55000'),
+(14, 2, 1, '2020-12-24', '50000'),
+(15, 2, 1, '2020-12-24', '40000'),
+(16, 2, 1, '2020-12-24', '35000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembelian_produk`
+--
+
+CREATE TABLE `pembelian_produk` (
+  `id_pembelian_produk` int(11) NOT NULL,
+  `id_pembelian` int(11) NOT NULL,
+  `id_produk` int(11) NOT NULL,
+  `jumlah` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pembelian_produk`
+--
+
+INSERT INTO `pembelian_produk` (`id_pembelian_produk`, `id_pembelian`, `id_produk`, `jumlah`) VALUES
+(1, 1, 1, '10000'),
+(2, 2, 2, '30000'),
+(3, 5, 1, '2'),
+(4, 5, 6, '1'),
+(5, 5, 8, '1'),
+(6, 6, 1, '20000'),
+(7, 6, 6, '15000'),
+(8, 6, 8, '10000'),
+(9, 7, 1, '20000'),
+(10, 7, 8, '10000'),
+(11, 8, 1, '20000'),
+(12, 8, 8, '10000'),
+(13, 9, 6, '15000'),
+(14, 10, 6, '15000'),
+(15, 11, 8, '10000'),
+(16, 11, 6, '15000'),
+(17, 11, 1, '20000'),
+(18, 12, 1, '20000'),
+(19, 12, 6, '15000'),
+(20, 13, 1, '20000'),
+(21, 13, 6, '15000'),
+(22, 14, 1, '20000'),
+(23, 14, 8, '10000'),
+(24, 15, 1, '20000'),
+(25, 16, 6, '15000');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `produk`
 --
 
@@ -66,6 +184,31 @@ INSERT INTO `produk` (`id_produk`, `id_jenis`, `nama_produk`, `harga_produk`, `b
 (6, 0, 'Sawi', '15000', '1000', 'Sawi.jpg', 'sayur sawi murah'),
 (8, 0, 'Daun Bawang', '10000', '1000', 'daun bawang.jpg', 'Daun bawang segar');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int(11) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `no_hp` varchar(15) NOT NULL,
+  `alamat` text NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id_user`, `id_kategori`, `email`, `nama`, `no_hp`, `alamat`, `password`) VALUES
+(1, 1, 'mohamadhidayatullah@gmail.com', 'mohammad hidayatullah', '082301878744', 'Pamekarasa, Jawa Timur', '123'),
+(2, 2, 'rian@gmail.com', 'rian admojo', '082333344555', 'jalan jalan', '123'),
+(3, 2, 'yayang@gmail.com', 'Yayang', '087123456789', 'jalan2', '1234');
+
 --
 -- Indexes for dumped tables
 --
@@ -77,10 +220,40 @@ ALTER TABLE `jenis`
   ADD PRIMARY KEY (`id_jenis`);
 
 --
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `ongkir`
+--
+ALTER TABLE `ongkir`
+  ADD PRIMARY KEY (`id_ongkir`);
+
+--
+-- Indexes for table `pembelian`
+--
+ALTER TABLE `pembelian`
+  ADD PRIMARY KEY (`id_pembelian`);
+
+--
+-- Indexes for table `pembelian_produk`
+--
+ALTER TABLE `pembelian_produk`
+  ADD PRIMARY KEY (`id_pembelian_produk`);
+
+--
 -- Indexes for table `produk`
 --
 ALTER TABLE `produk`
   ADD PRIMARY KEY (`id_produk`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -93,10 +266,40 @@ ALTER TABLE `jenis`
   MODIFY `id_jenis` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT for table `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `ongkir`
+--
+ALTER TABLE `ongkir`
+  MODIFY `id_ongkir` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `pembelian`
+--
+ALTER TABLE `pembelian`
+  MODIFY `id_pembelian` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `pembelian_produk`
+--
+ALTER TABLE `pembelian_produk`
+  MODIFY `id_pembelian_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
 -- AUTO_INCREMENT for table `produk`
 --
 ALTER TABLE `produk`
   MODIFY `id_produk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
