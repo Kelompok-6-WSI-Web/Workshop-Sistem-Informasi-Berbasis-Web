d781f98e614ef43d5003664ddcb22a66fce8f587
MohammadHidayatullah <mohammadhidayatullah85@gmail.com>
2021-01-08 16:36:11 +0700

    update query register sensitif jika dihosting

--- Web_Hi-Store/register_process.php
+++ Web_Hi-Store/register_process.php
@@ -1,17 +1,26 @@
 <?php 
 
-	include_once 'koneksi.php';
-    
-    $id_kategori = 2;
-    $email = $_POST['email'];
-    $nama = $_POST['nama'];
-    $no_hp = $_POST['no_hp'];
-    $alamat = $_POST['alamat'];
-	$password = $_POST['password'];
-	
+include_once 'koneksi.php';
 
-    mysqli_query($koneksi,"INSERT INTO users VALUES ('','$id_kategori','$email','$nama','$no_hp','$alamat','$password')");
+$id_kategori = 2;
+$email = $_POST['email'];
+$nama = $_POST['nama'];
+$no_hp = $_POST['no_hp'];
+$alamat = $_POST['alamat'];
+$password = $_POST['password'];
 
 
-	header("location:login.php?pesan=input");
+$query = $koneksi->query("INSERT INTO `users` (`id_kategori`, `email`, `nama`, `no_hp`, `alamat`, `password`) 
+                        VALUES ('$id_kategori', '$email', '$nama', '$no_hp', '$alamat', '$password')");
+
+// UNTUK CEK QUERY APAKAH BISA DIJALANKAN
+// if (!$query) {
+//     die("Query gagal dijalankan: " . mysqli_errno($koneksi) .
+// " - " . mysqli_error($koneksi));
+// } else {
+//     echo "halo";
+// }
+
+
+header("location:login.php?pesan=input");
 ?>
